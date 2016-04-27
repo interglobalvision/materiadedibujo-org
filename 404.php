4645fd762eee8593e0bd51e53d4078a41484e862
Patrick Best <patrickbest@patrickbest.com>
2016-04-26 20:15:09 -0500

    scaffold index and 404

--- 404.php
+++ 404.php
@@ -4,9 +4,12 @@ get_header();
 
 <!-- main content -->
 
-<section id="main-content">
-  <h1>Not found</h1>
-  <p>:(</p>
+<section id="main-content" class="container">
+  <div class="row">
+    <div class="col col-12">
+      <h3>404 Not found</h3>
+    </div>
+  </div>
 <!-- end main-content -->
 </section>
 

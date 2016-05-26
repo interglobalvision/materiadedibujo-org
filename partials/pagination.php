c242e91bd1866b947fe96e5d3136cdb80bfee27b
Patrick Best <patrickbest@patrickbest.com>
2016-05-25 19:54:42 -0500

    localize pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -4,8 +4,8 @@ if( get_next_posts_link() || get_previous_posts_link() ) {
   <!-- post pagination -->
   <nav id="pagination">
 <?php
-$previous = get_previous_posts_link('Newer');
-$next = get_next_posts_link('Older');
+$previous = get_previous_posts_link(__('[:es]Más Nuevo[:en]Newer'));
+$next = get_next_posts_link(__('[:es]Más Viejo[:en]Older'));
 if ($previous) {
   echo $previous;
 }

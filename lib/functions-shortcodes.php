f493a02ab1f1b344faa7721a662fbe897d4794d1
cas <cas@interglobal.vision>
2016-09-14 14:44:16 -0500

    add Bibliografia link at the bottom of the index

--- lib/functions-shortcodes.php
+++ lib/functions-shortcodes.php
@@ -33,6 +33,11 @@ function get_laboratorio_index() {
         $output .= '</ul></li>';
       }
     }
+
+    // Add link to bibliography
+    $bibliography = get_page_by_title('bibliografia');
+    $output.= '<li><a href="' . get_permalink($bibliography->ID)  . '"><strong>[:es]Bibliografía[:en]Bibliography</a></strong></li>';
+
     $output .= '</ul>';
 
     wp_reset_postdata();
